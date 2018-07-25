59475caa5ae48bc84ea410bfbd07b759465eecb1
Ylrahc_Play <ylrahcplay@gmail.com>
2018-07-25 15:27:05 +0200

    maj trigger

--- BD_Equipement/sql/4_Fonction.sql
+++ BD_Equipement/sql/4_Fonction.sql
@@ -40,11 +40,9 @@ $$ LANGUAGE plpgsql;
 -- Panneau Fonction Rec
 CREATE OR REPLACE FUNCTION bd_equipement.panneau(modif BOOLEAN, this_geom GEOMETRY, id INT, type INT, precis VARCHAR, date_amgt DATE, etat INT, commentaire TEXT) RETURNS VOID AS $$
 DECLARE
-	time_record TIMESTAMP;
 	nb_fichier 	INTEGER;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.panneau
@@ -56,25 +54,42 @@ BEGIN
 		WHERE pann_id = id ;
 
 	ELSE
-		INSERT INTO bd_equipement.panneau(pann_date_enre, pann_type_pann_id, pann_prec, pann_date_amgt, pann_etat_comm_id, pann_comm, pann_geom)
-		VALUES(time_record, type, precis, date_amgt, etat, commentaire, this_geom);
+		INSERT INTO bd_equipement.panneau(pann_type_pann_id, pann_prec, pann_date_amgt, pann_etat_comm_id, pann_comm, pann_geom)
+		VALUES(type, precis, date_amgt, etat, commentaire, this_geom);
 
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
 -- Panneau Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_panneau() RETURNS TRIGGER AS $$
-DECLARE
-	id_panneau 	INTEGER;
 
 BEGIN
 
-	SELECT pann_id INTO id_panneau FROM bd_equipement.panneau ORDER BY pann_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT pann_id AS id, pann_geom AS geom
+		FROM bd_equipement.panneau
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.panneau
-	SET pann_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), pann_geom) AND pann_id = id_panneau AND categorie = 1;
-
+	SET pann_site_cen_id = equip_site."ID"
+	  , pann_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE pann_id = rec.id
+	;
+	
 	RETURN NULL;
 
 END;
@@ -86,11 +101,9 @@ FOR EACH ROW EXECUTE PROCEDURE bd_equipement.site_panneau();
 -- Sentier Fonction Rec
 CREATE OR REPLACE FUNCTION bd_equipement.sentier(modif BOOLEAN, this_geom GEOMETRY, id INT, type_sentier INT, type_cheminement INT, date_amgt DATE, etat INT, pmr BOOLEAN, difficulte INT, commentaire TEXT) RETURNS VOID AS $$
 DECLARE
-	time_record	TIMESTAMP;
 	nb_fichier	INTEGER;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.sentier
@@ -104,24 +117,41 @@ BEGIN
         WHERE sent_id = id;
 
 	ELSE
-		INSERT INTO bd_equipement.sentier(sent_date_enre, sent_type_sent_id, sent_type_chem_id, sent_date_amgt, sent_etat_comm_id, sent_acces_pmr, sent_diff_id, sent_comm, sent_geom)
-		VALUES(time_record, type_sentier, type_cheminement, date_amgt, etat, pmr, difficulte, commentaire, this_geom);
+		INSERT INTO bd_equipement.sentier(sent_type_sent_id, sent_type_chem_id, sent_date_amgt, sent_etat_comm_id, sent_acces_pmr, sent_diff_id, sent_comm, sent_geom)
+		VALUES(type_sentier, type_cheminement, date_amgt, etat, pmr, difficulte, commentaire, this_geom);
 
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
 -- Sentier Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_sentier() RETURNS TRIGGER AS $$
-DECLARE
-	id_sentier	INTEGER;
 
 BEGIN
 
-	SELECT sent_id INTO id_sentier FROM bd_equipement.sentier ORDER BY sent_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT sent_id AS id, sent_geom AS geom
+		FROM bd_equipement.sentier
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.sentier
-	SET sent_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), sent_geom) AND sent_id = id_sentier AND categorie = 1;
+	SET sent_site_cen_id = equip_site."ID"
+	  , sent_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE sent_id = rec.id
+	;
 
 	RETURN NULL;
 
@@ -133,39 +163,53 @@ FOR EACH ROW EXECUTE PROCEDURE bd_equipement.site_sentier();
 
 -- Autre Aménagement de Valorisation Fonction Rec
 CREATE OR REPLACE FUNCTION bd_equipement.autre_amenagement_communication(modif BOOLEAN, this_geom GEOMETRY, id INT, type INT, date_amgt DATE, etat INT, commentaire TEXT) RETURNS VOID AS $$
-DECLARE
-	time_record			TIMESTAMP;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.autre_amenagement_communication
         SET autr_amen_comm_type_autr_amen_comm_id = type
-				  , autr_amen_comm_date_amgt = date_amgt
-				  , autr_amen_comm_etat_comm_id = etat
-				  , autr_amen_comm_comm = commentaire
+		  , autr_amen_comm_date_amgt = date_amgt
+		  , autr_amen_comm_etat_comm_id = etat
+		  , autr_amen_comm_comm = commentaire
         WHERE autr_amen_comm_id = id;
 
 	ELSE
-		INSERT INTO bd_equipement.autre_amenagement_communication(autr_amen_comm_date_enre, autr_amen_comm_type_autr_amen_comm_id, autr_amen_comm_date_amgt, autr_amen_comm_etat_comm_id, autr_amen_comm_comm, autr_amen_comm_geom)
-		VALUES(time_record, type, date_amgt, etat, commentaire, this_geom);
+		INSERT INTO bd_equipement.autre_amenagement_communication(autr_amen_comm_type_autr_amen_comm_id, autr_amen_comm_date_amgt, autr_amen_comm_etat_comm_id, autr_amen_comm_comm, autr_amen_comm_geom)
+		VALUES(type, date_amgt, etat, commentaire, this_geom);
 
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
 -- Autre Aménagement de Valorisation Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_autre_amenagement_communication() RETURNS TRIGGER AS $$
-DECLARE
-	id_autr_amen_comm	INTEGER;
 
 BEGIN
 
-	SELECT autr_amen_comm_id INTO id_autr_amen_comm FROM bd_equipement.autre_amenagement_communication ORDER BY autr_amen_comm_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT autr_amen_comm_id AS id, autre_amen_comm_geom AS geom
+		FROM bd_equipement.autre_amenagement_communication
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.autre_amenagement_communication
-	SET autr_amen_comm_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), autr_amen_comm_geom) AND autr_amen_comm_id = id_autr_amen_comm AND categorie = 1;
+	SET autr_amen_comm_site_cen_id = equip_site."ID"
+	  , autr_amen_comm_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE autr_amen_comm_site_cen_id = rec.id
+	;
 
 	RETURN NULL;
 
@@ -177,11 +221,8 @@ FOR EACH ROW EXECUTE PROCEDURE bd_equipement.site_autre_amenagement_communicatio
 
 -- Clôture Fonction Rec
 CREATE OR REPLACE FUNCTION bd_equipement.cloture(modif BOOLEAN, this_geom GEOMETRY, id INT, type_mobilite INT, type_fils INT, type_poteau INT, date_amgt DATE, partiel BOOLEAN, etat INT, commentaire TEXT) RETURNS VOID AS $$
-DECLARE
-	time_record	TIMESTAMP;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.cloture
@@ -191,28 +232,45 @@ BEGIN
           , clot_date_amgt = date_amgt
           , clot_partiel = partiel
           , clot_etat_zoot_id = etat
-				  , clot_comm = commentaire
+		  , clot_comm = commentaire
         WHERE clot_id = id;
 
 	ELSE
-		INSERT INTO bd_equipement.cloture(clot_date_enre, clot_type_mobi_id , clot_type_fils_id , clot_type_pote_id , clot_date_amgt , clot_partiel , clot_etat_zoot_id, clot_comm, clot_geom)
-		VALUES(time_record, type_mobilite, type_fils, type_poteau, date_amgt, partiel, etat, commentaire, this_geom);
+		INSERT INTO bd_equipement.cloture(clot_type_mobi_id , clot_type_fils_id , clot_type_pote_id , clot_date_amgt , clot_partiel , clot_etat_zoot_id, clot_comm, clot_geom)
+		VALUES(type_mobilite, type_fils, type_poteau, date_amgt, partiel, etat, commentaire, this_geom);
 
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
 -- Clôture Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_cloture() RETURNS TRIGGER AS $$
-DECLARE
-	id_cloture	INTEGER;
 
 BEGIN
 
-	SELECT clot_id INTO id_cloture FROM bd_equipement.cloture ORDER BY clot_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT clot_id AS id, clot_geom AS geom
+		FROM bd_equipement.cloture
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.cloture
-	SET clot_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), clot_geom) AND clot_id = id_cloture AND categorie = 1;
+	SET clot_site_cen_id = equip_site."ID"
+	  , clot_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE clot_id = rec.id
+	;
 
 	RETURN NULL;
 
@@ -226,10 +284,8 @@ FOR EACH ROW EXECUTE PROCEDURE bd_equipement.site_cloture();
 CREATE OR REPLACE FUNCTION bd_equipement.barriere(modif BOOLEAN, this_geom GEOMETRY, id INT, type_mobilite INT, type_structure INT, dimension INT, date_amgt DATE, cadenasPerm BOOLEAN, etat INT, commentaire TEXT) RETURNS INT AS $$
 DECLARE
 	distance	INTEGER;
-	time_record	TIMESTAMP;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.barriere
@@ -254,8 +310,8 @@ BEGIN
 			END IF;
 		END IF;
 
-		INSERT INTO bd_equipement.barriere(barr_date_enre, barr_type_mobi_id, barr_type_stru_id, barr_dime, barr_date_amgt, barr_cade_perm, barr_etat_zoot_id, barr_comm, barr_geom)
-		VALUES(time_record, type_mobilite, type_structure, dimension, date_amgt, cadenasPerm, etat, commentaire, this_geom);
+		INSERT INTO bd_equipement.barriere(barr_type_mobi_id, barr_type_stru_id, barr_dime, barr_date_amgt, barr_cade_perm, barr_etat_zoot_id, barr_comm, barr_geom)
+		VALUES(type_mobilite, type_structure, dimension, date_amgt, cadenasPerm, etat, commentaire, this_geom);
 
 	END IF;
 
@@ -264,16 +320,33 @@ END;
 $$ LANGUAGE plpgsql;
 -- Barrière Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_barriere() RETURNS TRIGGER AS $$
-DECLARE
-	id_barriere	INTEGER;
 
 BEGIN
 
-	SELECT barr_id INTO id_barriere FROM bd_equipement.barriere ORDER BY barr_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT barr_id AS id, barr_geom AS geom
+		FROM bd_equipement.barriere
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.barriere
-	SET barr_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), barr_geom) AND barr_id = id_barriere AND categorie = 1;
+	SET barr_site_cen_id = equip_site."ID"
+	  , barr_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE barr_id = rec.id
+	;
 
 	RETURN NULL;
 
@@ -286,11 +359,9 @@ FOR EACH ROW EXECUTE PROCEDURE bd_equipement.site_barriere();
 -- Autre Aménagement de Gestion Fonction Rec
 CREATE OR REPLACE FUNCTION bd_equipement.autre_amenagement_zootechnie(modif BOOLEAN, this_geom GEOMETRY, id INT, type INT, date_amgt DATE, etat INT, commentaire TEXT) RETURNS INT AS $$
 DECLARE
-	distance			INTEGER;
-	time_record			TIMESTAMP;
+	distance	INTEGER;
 
 BEGIN
-	time_record := now();
 
 	IF (modif = TRUE) THEN
 		UPDATE bd_equipement.autre_amenagement_zootechnie
@@ -312,8 +383,8 @@ BEGIN
 			END IF;
 		END IF;
 
-		INSERT INTO bd_equipement.autre_amenagement_zootechnie(autr_amen_zoot_date_enre, autr_amen_zoot_type_autr_amen_zoot_id, autr_amen_zoot_date_amgt, autr_amen_zoot_etat_zoot_id, autr_amen_zoot_comm, autr_amen_zoot_geom)
-		VALUES(time_record, type, date_amgt, etat, commentaire, this_geom);
+		INSERT INTO bd_equipement.autre_amenagement_zootechnie(autr_amen_zoot_type_autr_amen_zoot_id, autr_amen_zoot_date_amgt, autr_amen_zoot_etat_zoot_id, autr_amen_zoot_comm, autr_amen_zoot_geom)
+		VALUES(type, date_amgt, etat, commentaire, this_geom);
 
 	END IF;
 
@@ -321,19 +392,35 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
-
 -- Autre Aménagement de Gestion Trigger Site
 CREATE OR REPLACE FUNCTION bd_equipement.site_autre_amenagement_zootechnie() RETURNS TRIGGER AS $$
-DECLARE
-	id_autr_amen_zoot	INTEGER;
 
 BEGIN
 
-	SELECT autr_amen_zoot_id INTO id_autr_amen_zoot FROM bd_equipement.autre_amenagement_zootechnie ORDER BY autr_amen_zoot_id DESC LIMIT 1;
-
+	WITH enregistrement AS (
+		SELECT autr_amen_zoot_id AS id, autre_amen_zoot_geom AS geom
+		FROM bd_equipement.autre_amenagement_zootechnie
+		ORDER BY id DESC
+		LIMIT 1
+	),
+	site AS (
+		SELECT "ID", geom, ST_Buffer(ST_Simplify(geom, 0.1), 100) AS buffer_geom
+		FROM md.site_cenhn
+		WHERE categorie = 1
+	),
+	equip_site AS (
+		SELECT "ID", ST_Distance(rec.geom, site.geom) AS dist
+		FROM enregistrement AS rec, site
+		WHERE ST_Intersects(rec.geom, site.buffer_geom)
+		ORDER BY dist
+		LIMIT 1
+	)
 	UPDATE bd_equipement.autre_amenagement_zootechnie
-	SET autr_amen_zoot_site_cen_id = site_cenhn."ID" FROM md.site_cenhn
-	WHERE ST_Intersects(ST_Buffer(geom, 100), autr_amen_zoot_geom) AND autr_amen_zoot_id = id_autr_amen_zoot AND categorie = 1;
+	SET autr_amen_zoot_site_cen_id = equip_site."ID"
+	  , autr_amen_zoot_date_enre = now()
+	FROM enregistrement AS rec, equip_site
+	WHERE autr_amen_zoot_site_cen_id = rec.id
+	;
 
 	RETURN NULL;
 
